5a1f6f122c1b9f18c495a58aa3eb8af331b555dd
vamshi <krishnavamshi423@gmail.com>
2024-01-06 01:48:53 +0000

    removing errors

--- 02.variable.sh
+++ 02.variable.sh
@@ -3,7 +3,7 @@
 PERSON1=RAMESH
 PERSON2=SURESH
 
-echo "$PERSON1" : "Hellow $PESRON2, Good Morning"
-echo "$PERSON2" : "Hellow $PESRSON1, Very Good Monring"
+echo "$PERSON1" : "Hellow $PERSON2, Good Morning"
+echo "$PERSON2" : "Hellow $PERSON1, Very Good Monring"
 echo "$PERSON1" : "How are you $PERSON2"
 echo "$PERSON2" : "i am good thanks $PERSON1"
