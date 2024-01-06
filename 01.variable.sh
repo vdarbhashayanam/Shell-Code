1d59749d3a0f49660b357ba4fe0e9295ea3f27dc
vamshi <krishnavamshi423@gmail.com>
2024-01-06 01:36:05 +0000

    update code without errors

--- 01.variable.sh
+++ 01.variable.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-Echo "Ramesh" : "Hellow "Suresh", Good Morning"
-Echo "Suresh" : "Hellow "Ramhesh", Very Good Monring"
-Echo "Ramesh" : "How are you "Suresh"
-Echo "Suresh" : "i am good thanks "Ramesh" "
+echo "Ramesh" : "Hellow Suresh, Good Morning"
+echo "Suresh" : "Hellow Ramhesh, Very Good Monring"
+echo "Ramesh" : "How are you Suresh"
+echo "Suresh" : "i am good thanks Ramesh"
