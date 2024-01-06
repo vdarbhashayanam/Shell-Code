84fe0bdf455cf4746e96ecb6c5bdb1c29f43ac0b
vamshi <krishnavamshi423@gmail.com>
2024-01-06 00:19:05 +0000

    this is hopefully sucess

---
+++ 01.variable.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+Echo "Ramesh" : "Hellow Suresh, Good Morning"
+Echo "Suresh" : "Hellow Ramhesh, Very Good Monring"
+Echo "Ramesh" : "How are you Ramesh"
+Echo "Suresh" : "i am good thanks"
