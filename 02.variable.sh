981163d921dd9caba155c128317b11ca6742695c
vamshi <krishnavamshi423@gmail.com>
2024-01-06 01:45:38 +0000

    2nd script

---
+++ 02.variable.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1" : "Hellow $PESRON2, Good Morning"
+echo "$PERSON2" : "Hellow $PESRSON1, Very Good Monring"
+echo "$PERSON1" : "How are you $PERSON2"
+echo "$PERSON2" : "i am good thanks $PERSON1"
